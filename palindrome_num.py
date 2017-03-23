e57a7319f4a6b21572c32366da3d5172a9f1dcb2
Jasmine Dahilig <jasmine@factual.com>
2017-03-23 09:28:29 -0700

    finished rough palindrome numbers w/ string reversal

--- palindrome_num.py
+++ palindrome_num.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
-END = 999
-i = 100
-j = 100
 
-while i < END and j < END:
+HI = 999
+LO = 99
+i = HI
+j = HI
+
+palindromes = []
+
+# does unnecessary iterations, does it twice for mirror numbers
+while i > LO:
+  while j > LO:
+    n = i * j
+    # feel like there should be a way to do this w/ math...
+    n_str = str(n)
+    if n_str[::-1] == n_str:
+      palindromes.append(n)
+      break
+    j = j - 1
+  j = HI
+  i = i - 1
+
+palindromes.sort()
+print palindromes[-1]
